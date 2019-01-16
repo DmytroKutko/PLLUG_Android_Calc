8efa118c85321b9582fcdae59857986438d7e940
Dmytro Kutko <dmytrokutko@gmail.com>
2019-01-16 10:14:37 +0200

    round fix

--- app/src/main/java/ua/com/zzz/dmytrokutko/pllug_calc/Calc.java
+++ app/src/main/java/ua/com/zzz/dmytrokutko/pllug_calc/Calc.java
@@ -3,6 +3,9 @@ package ua.com.zzz.dmytrokutko.pllug_calc;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Calc {
 
     public static strictfp String eval(String expression) {
@@ -11,7 +14,8 @@ public class Calc {
         try {
             Expression e = new ExpressionBuilder(expression).build();
             val = e.evaluate();
-            res = String.format("%.2f", val);
+            val = new BigDecimal(val).setScale(2, RoundingMode.UP).doubleValue();
+            res = String.valueOf(val);
         } catch (Exception e){
             res = "";
         }
